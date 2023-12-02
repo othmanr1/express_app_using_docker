2834616a17e387ca9ff8b07c793579837894b364
othmanr1 <rothman855@gmail.com>
2023-12-02 12:19:28 +0100

    docker conf

--- routes/role.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const route = express.Router()
-const role = require('../controllers/role.controller')
-
-route.get('/find', role.findAll);
-route.post('/Add', role.create);
-
-module.exports = route;
--- routes/user.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const route = express.Router()
-const usr = require('../controllers/user.controller')
-
-route.post('/sub', usr.subscribe);
-route.post('/login', usr.login);
-
-module.exports = route;
